689dcf63c99c3e37feb67e367833647326992389
r_code_explore <rciupercher@gmail.com>
2022-02-24 21:02:34 +0100

    front integration start

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$title = "ed_admin";
+
+include 'html/header.html';
+
+?>
+
+<div class="index-intro">
+    <h1 class="index-title">Ed_Admin | Full-stack Web Developper</h1>
+</div>
+
+<?php
+
+include 'html/footer.html';
+
+?>
