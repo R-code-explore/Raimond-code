48855e0ef5d87d89780b1e72db92a836aa59f6d0
r_code_explore <rciupercher@gmail.com>
2022-03-17 17:01:32 +0100

    Commit the contact page

---
+++ php/contact.php
@@ -0,0 +1,86 @@
+<?php
+
+//require 'connect.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--author and description-->
+    <meta name="author" content="Eduard Raimond C.">
+    <meta name="description" content="Fullstack web developer, développeur web fullstack, Portfolio website, site web portfolio">
+
+    <!--favicon-->
+    <link rel="shortcut icon" href="../assets/logo-raimond-code.png" type="image/x-icon">
+
+    <!--CSS-->
+    <link rel="stylesheet" type="text/css" href="../css/contact-style.css">
+
+	<title>Contact</title>
+</head>
+<body>
+
+	<h1>Contact</h1>
+
+	<form class="form" action="" method="post">	
+
+		<div>
+			<label for="name">First name & last name</label>
+			<input type="text" id="name" name="name" required>
+		</div>
+
+		<div>
+			<label for="email">Your email adress</label>
+			<input type="email" id="email" name="email" required>
+		</div>
+
+		<div>
+			<label for="phone">Phone number</label>
+			<input type="tel" id="phone" name="phone" required>
+		</div>
+
+		<div>
+			<label for="msg">Your request</label>
+			<textarea type="text" class="msg" name="msg" required></textarea>
+		</div>
+
+		<button type="submit">Send message</button>
+
+	</form>
+
+	<footer>
+            <div>
+                <ul>
+                    <li><a href="https://github.com/R-code-explore">Github</a></li>
+                    <li><a href="https://www.linkedin.com/in/eduard-ciupercher-8329a61a6">LinkedIn</a></li>
+                    <li><a href="">Blog</a></li>
+                </ul>
+            </div>
+
+            <aside>
+                <ul>
+                    <li><a href="../cgu/cgu.php">Terms and conditions of use</a></li>
+                    <li><a href="">F.A.Q.</a></li>
+                </ul>
+            </aside>
+        </footer>
+
+	<style>
+		h1{
+			text-align: center;
+			margin-top: 50px;
+			margin-bottom: 20px;
+			color: #00FF29;
+
+			font-size: 2rem;
+		}
+	</style>
+
+</body>
+</html>
---
+++ php/thx-msg.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <meta name="author" content="Eduard Raimond C.">
+    <meta name="description" content="Fullstack web developer, développeur web fullstack, Portfolio website, site web portfolio">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!--favicon-->
+    <link rel="shortcut icon" href="../assets/logo-raimond-code.png" type="image/x-icon">
+
+    <!--css-->
+    <link rel="stylesheet" type="text/css" href="../css/contact-style.css">
+
+    <title>Thanks !</title>
+</head>
+<body>  
+
+    <h1 id="thx-msg">Thank you !</h1>
+
+    <a href="../index.html">Retour accueil</a>
+
+    <style>
+        #thx-msg{
+    position: relative;
+    top: 100px;
+    left: 50%;
+    transform: translateX(-50%);
+
+    text-align: center;
+    color: #00FF29;
+    margin: 100px auto;
+}
+
+    a{
+        display: block;
+        margin: 200px auto;
+        width: 200px;
+
+        text-align: center;
+        padding: 15px;
+        border: none;
+        text-decoration: none;
+        color: #00FF29;
+
+        background: rgba(255, 255, 255, 0.05);
+    background-blend-mode: lighten;
+    box-shadow: 10px 20px 20px rgba(0, 0, 0, 0.25), -5px -10px 20px rgba(0, 56, 255, 0.16), inset 10px 20px 30px rgba(0, 0, 0, 0.42), inset -10px -10px 20px rgba(28, 105, 255, 0.25);
+    border-radius: 44px;
+    }
+    </style>
+
+</body>
+</html>
